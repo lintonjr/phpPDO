dfcf30af6233bbd41b5a04b4892364b9a1937e53
lintonjr <lintonjr@hotmail.com>
2018-08-09 14:50:34 -0400

    Conexão

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
-phpinfo();
+// phpinfo();
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=test_oo", "root", "root");
+} catch (PDOException $e) {
+    echo "error! Message: " . $e->getMessage() . " Error Code: " . $e->getCode();
+}
