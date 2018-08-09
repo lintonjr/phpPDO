868faa9212cf84830c937cc92414a8e59b7075a2
lintonjr <lintonjr@hotmail.com>
2018-08-09 15:18:15 -0400

    test conn and query

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 // phpinfo();
 try {
     $conn = new PDO("mysql:host=localhost;dbname=test_oo", "root", "root");
+
+    // $query = "SELECT * FROM `products`";
+    $query = "insert into `products` (`name`, `desc`) values ('eBook', 'Learn JS components')";
+    $return = $conn->exec($query);
+    print_r($return);
+
 } catch (PDOException $e) {
     echo "error! Message: " . $e->getMessage() . " Error Code: " . $e->getCode();
 }
