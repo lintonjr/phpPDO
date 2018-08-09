6781a6ab8107d324ab9431c2f967b408b9ba3c43
lintonjr <lintonjr@hotmail.com>
2018-08-09 15:23:25 -0400

    Fetchs

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
 try {
     $conn = new PDO("mysql:host=localhost;dbname=test_oo", "root", "root");
 
-    // $query = "SELECT * FROM `products`";
-    $query = "insert into `products` (`name`, `desc`) values ('eBook', 'Learn JS components')";
-    $return = $conn->exec($query);
-    print_r($return);
+    $query = "SELECT * FROM `products`";
+    // $query = "insert into `products` (`name`, `desc`) values ('eBook', 'Learn JS components')";
+    $stmt = $conn->query($query);
+    $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // echo $list[0]['name'];
+    print_r($list);
 
 } catch (PDOException $e) {
     echo "error! Message: " . $e->getMessage() . " Error Code: " . $e->getCode();
