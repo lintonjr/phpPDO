f1c542f0cc633599a13f1857d89d03acdf74210a
lintonjr <lintonjr@hotmail.com>
2018-08-09 20:29:32 -0400

    Inseridas as Interfaces e Classes de Produto e o serviço de produtos com os metodos

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 require_once "IConn.php";
 require_once "Conn.php";
+require_once "IProduct.php";
+require_once "Product.php";
+require_once "ServiceProduct.php";
 // phpinfo();
 
 $db = new Conn("localhost", "test_oo", "root", "root");
