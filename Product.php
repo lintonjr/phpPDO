04ab941dc812648d77291b65dc2c1006d77959c2
lintonjr <lintonjr@hotmail.com>
2018-08-09 20:30:55 -0400

    Arquivos de Interface e classe de produtos e Serviço de produtos

---
+++ Product.php
@@ -0,0 +1,68 @@
+<?php
+
+class Product implements IProduct
+{
+    private $id;
+    private $name;
+    private $desc;
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of desc
+     */
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+
+    /**
+     * Set the value of desc
+     *
+     * @return  self
+     */
+    public function setDesc($desc)
+    {
+        $this->desc = $desc;
+
+        return $this;
+    }
+}
